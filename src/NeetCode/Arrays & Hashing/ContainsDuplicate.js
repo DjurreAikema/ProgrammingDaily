98e4455f93c2bb0d66101b127541d43dab027e85
d.aikema <djurreaikema@gmail.com>
2024-12-06 14:40:47 +0100

    06-12-2024

---
+++ src/NeetCode/Arrays & Hashing/ContainsDuplicate.js
@@ -0,0 +1,11 @@
+class Solutions {
+  hasDuplicate061224(nums) {
+    for (let i = 0; i < nums.length - 1; i++) {
+      for (let j = i + 1; j < nums.length; j++) {
+        if (nums[i] === nums[j]) return true;
+      }
+    }
+
+    return false;
+  }
+}
