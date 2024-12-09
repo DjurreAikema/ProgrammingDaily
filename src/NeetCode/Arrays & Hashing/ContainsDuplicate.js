4b60e593ebbbf8fbf9c03916bf83aa99d6e7993a
Djurre <djurreaikema@gmail.com>
2024-12-09 13:45:05 +0100

    091124

--- src/NeetCode/Arrays & Hashing/ContainsDuplicate.js
+++ src/NeetCode/Arrays & Hashing/ContainsDuplicate.js
@@ -1,4 +1,9 @@
 class Solutions {
+
+  hasDuplicate091124(nums) {
+    return new Set(nums).size < nums.length;
+  }
+
   hasDuplicate061224(nums) {
     for (let i = 0; i < nums.length - 1; i++) {
       for (let j = i + 1; j < nums.length; j++) {
