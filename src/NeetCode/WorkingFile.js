92a964c3fc4214d4e0a1ab0ec3e16b6de1815a9a
d.aikema <djurreaikema@gmail.com>
2024-12-10 14:20:15 +0100

    10-12-2024

--- src/NeetCode/WorkingFile.js
+++ src/NeetCode/WorkingFile.js
@@ -1,23 +1,8 @@
+import {alphaNum} from "../Helpers/Helpers.js";
+
 //noinspection Duplicates
 class Solution {
-  /**
-   * @param {number[]} nums
-   * @param {number} target
-   * @return {number[]}
-   */
-  twoSum101224(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-      for (let j = i + 1; j < nums.length; j++) {
-        if (i === j) continue;
 
-        const num = nums[i] + nums[j];
-        if (num === target) return [i, j];
-      }
-    }
-  }
 }
 
 const sol = new Solution();
-console.log(sol.twoSum([3, 4, 5, 6], 7)); // [0,1]
-console.log(sol.twoSum([4, 5, 6], 10)); // [0,2]
-console.log(sol.twoSum([5, 5], 10)); // [0,1]
