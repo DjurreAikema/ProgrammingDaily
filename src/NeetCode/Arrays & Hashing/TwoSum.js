efa6dfff9890a328cc4c7e3323c035db4db9fb45
d.aikema <djurreaikema@gmail.com>
2024-12-10 11:56:53 +0100

    10-12-2024

--- src/NeetCode/Arrays & Hashing/TwoSum.js
+++ src/NeetCode/Arrays & Hashing/TwoSum.js
@@ -1,6 +1,23 @@
 //noinspection Duplicates
 class Solutions {
 
+  twoSum101224_3(nums, target) {
+    const indices = {}; // val -> index
+
+    for (let i = 0; i < nums.length; i++) {
+      indices[nums[i]] = i;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+      let diff = target - nums[i];
+      if (indices[diff] !== undefined && indices[diff] !== i) {
+        return [i, indices[diff]];
+      }
+    }
+
+    return [];
+  }
+
   twoSum101224_2(nums, target) {
     let A = [];
     for (let i = 0; i < nums.length; i++) {
