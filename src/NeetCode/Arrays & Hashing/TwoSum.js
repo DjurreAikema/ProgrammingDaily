d63c55936408df87e3687f7c4ed88d1c40f1f57f
d.aikema <djurreaikema@gmail.com>
2024-12-10 11:52:45 +0100

    10-12-2024

--- src/NeetCode/Arrays & Hashing/TwoSum.js
+++ src/NeetCode/Arrays & Hashing/TwoSum.js
@@ -1,6 +1,32 @@
 //noinspection Duplicates
 class Solutions {
 
+  twoSum101224_2(nums, target) {
+    let A = [];
+    for (let i = 0; i < nums.length; i++) {
+      A.push([nums[i], i]);
+    }
+
+    A.sort((a, b) => a[0] - b[0]);
+
+    let i = 0, j = nums.length - 1;
+    while (i < j) {
+      let cur = A[i][0] + A[j][0];
+      if (cur === target) {
+        return [
+          Math.min(A[i][1], A[j][1]),
+          Math.max(A[i][1], A[j][1])
+        ]
+      } else if (cur < target) {
+        i++;
+      } else {
+        j--;
+      }
+    }
+
+    return [];
+  }
+
   twoSum101224(nums, target) {
     for (let i = 0; i < nums.length; i++) {
       for (let j = i + 1; j < nums.length; j++) {
