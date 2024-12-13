49e9f30fb170513fc40a03de5caac8546a0e26d5
d.aikema <djurreaikema@gmail.com>
2024-12-13 11:43:47 +0100

    13-12-2024

--- src/NeetCode/Binary Search/BinarySearch.js
+++ src/NeetCode/Binary Search/BinarySearch.js
@@ -1,8 +1,42 @@
 //noinspection Duplicates
 class Solutions {
   // Best (so far)
+  search_best(nums, target) {
+    let l = 0, r = nums.length - 1;
 
+    while (l <= r) {
+      // Left + half the distance between L and R
+      let m = Math.floor(l + ((r - l) / 2));
+
+      if (nums[m] > target) {
+        r = m - 1;
+      } else if (nums[m] < target) {
+        l = m + 1;
+      } else {
+        return m;
+      }
+    }
+
+    return -1;
+  }
 
   // All solutions
+  search131224(nums, target) {
+    let l = 0, r = nums.length - 1;
 
+    while (l <= r) {
+      // Left + half the distance between L and R
+      let m = Math.floor(l + ((r - l) / 2));
+
+      if (nums[m] > target) {
+        r = m - 1;
+      } else if (nums[m] < target) {
+        l = m + 1;
+      } else {
+        return m;
+      }
+    }
+
+    return -1;
+  }
 }
