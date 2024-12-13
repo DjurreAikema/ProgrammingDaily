2f151232feac597cf54197ca53a6f21c27ce7bbd
d.aikema <djurreaikema@gmail.com>
2024-12-13 09:21:33 +0100

    13-12-2024

--- src/NeetCode/Sliding Window/BestTimeToBuyAndSellStock.js
+++ src/NeetCode/Sliding Window/BestTimeToBuyAndSellStock.js
@@ -20,6 +20,24 @@ class Solutions {
   }
 
   // All solutions
+  maxProfit131224(prices) {
+    let l = 0, r = 1;
+    let maxProfit = 0;
+
+    while (r < prices.length) {
+      if (prices[l] < prices[r]) {
+        const profit = prices[r] - prices[l];
+        maxProfit = Math.max(profit, maxProfit);
+      } else {
+        l = r;
+      }
+
+      r++;
+    }
+
+    return maxProfit;
+  }
+
   maxProfit121224(prices) {
     let l = 0, r = 1;
     let maxProfit = 0;
