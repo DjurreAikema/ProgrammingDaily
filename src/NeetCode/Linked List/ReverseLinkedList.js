96b7d018699ccd30882b89aed8d29b662eb1aa53
Djurre <djurreaikema@gmail.com>
2024-12-15 21:16:52 +0100

    15-12-2024

--- src/NeetCode/Linked List/ReverseLinkedList.js
+++ src/NeetCode/Linked List/ReverseLinkedList.js
@@ -15,6 +15,19 @@ class Solutions {
   }
 
   // All solutions
+  reverseList151224(head) {
+    let prev = null, curr = head;
+
+    while (curr) {
+      let tempNext = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = tempNext;
+    }
+
+    return prev;
+  }
+
   reverseList141224(head) {
     let prev = null, curr = head;
 
