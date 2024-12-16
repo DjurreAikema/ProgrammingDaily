fbd9379d98bcf3fe6e1872df17d555ff4dc51c5d
d.aikema <djurreaikema@gmail.com>
2024-12-16 13:17:29 +0100

    16-12-2024

--- src/NeetCode/Stack/ValidParentheses.js
+++ src/NeetCode/Stack/ValidParentheses.js
@@ -26,6 +26,30 @@ class Solutions {
   }
 
   // All solutions
+  isValid161224(s) {
+    const stack = [];
+    const closeToOpen = {
+      ')': '(',
+      ']': '[',
+      '}': '{'
+    }
+
+    for (let c of s) {
+      // Is c a closing character
+      if (closeToOpen[c]) {
+        if (stack.length > 0 && stack[stack.length - 1] === closeToOpen[c]) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      } else {
+        stack.push(c);
+      }
+    }
+
+    return stack.length === 0;
+  }
+
   isValid151224(s) {
     const stack = [];
     const closeToOpen = {
