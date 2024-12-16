18ff23f5dbeeaf2c535965bb91303d8b0ac79de4
d.aikema <djurreaikema@gmail.com>
2024-12-16 15:24:14 +0100

    16-12-2024

--- src/Helpers/TestCases.js
+++ src/Helpers/TestCases.js
@@ -6,11 +6,11 @@ import {arrayToList, listToArray} from "./DataStructures.js";
 export function reverseListTestCases(sol) {
   let list = arrayToList([0, 1, 2, 3]);
   let reversedList = sol.reverseList(list);
-  console.log(listToArray(reversedList));
+  console.log(listToArray(reversedList), "[3,2,1,0]");
 
   let list2 = arrayToList([]);
   let reversedList2 = sol.reverseList(list2);
-  console.log(listToArray(reversedList2));
+  console.log(listToArray(reversedList2), "[]");
 }
 
 /**
--- src/NeetCode/Linked List/ReverseLinkedList.js
+++ src/NeetCode/Linked List/ReverseLinkedList.js
@@ -15,6 +15,19 @@ class Solutions {
   }
 
   // All solutions
+  reverseList161224(head) {
+    let prev = null, curr = head;
+
+    while (curr) {
+      let tempNext = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = tempNext;
+    }
+
+    return prev;
+  }
+
   reverseList151224(head) {
     let prev = null, curr = head;
 
