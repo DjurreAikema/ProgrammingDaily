c1287de3595b0380b91a59bb737b1550840877d8
d.aikema <djurreaikema@gmail.com>
2024-12-23 13:10:26 +0100

    23-12-2024

--- src/NeetCode/Trees/InvertBinaryTree.js
+++ src/NeetCode/Trees/InvertBinaryTree.js
@@ -19,6 +19,23 @@ class Solutions {
   }
 
   // All solutions
+  invertTree231224(root) {
+    if (!root) return null;
+    // shift() for BFS, pop() for DFS
+    const queue = [root];
+
+    while (queue.length) {
+      const node = queue.pop();
+
+      [node.left, node.right] = [node.right, node.left];
+
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+
+    return root;
+  }
+
   invertTree221224(root) {
     if (!root) return null;
     const queue = [root];
