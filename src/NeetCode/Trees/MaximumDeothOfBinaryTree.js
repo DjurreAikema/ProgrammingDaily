2028d167f00ca5cb7de916023a66f37e160a320c
d.aikema <djurreaikema@gmail.com>
2024-12-23 13:16:09 +0100

    23-12-2024

--- src/NeetCode/Trees/MaximumDeothOfBinaryTree.js
+++ src/NeetCode/Trees/MaximumDeothOfBinaryTree.js
@@ -21,6 +21,25 @@ class Solutions {
   }
 
   // All solutions
+  maxDepth231224(root) {
+    const queue = [[root, 1]];
+    let maxDepth = 0;
+
+    while (queue.length) {
+      const curr = queue.pop();
+      const node = curr[0];
+      const depth = curr[1];
+
+      maxDepth = Math.max(depth, maxDepth);
+      if (node) {
+        if (node.left) queue.push([node.left, depth + 1]);
+        if (node.right) queue.push([node.right, depth + 1]);
+      }
+    }
+
+    return maxDepth;
+  }
+
   maxDepth221224(root) {
     const stack = [[root, 1]];
     let maxDepth = 0;
