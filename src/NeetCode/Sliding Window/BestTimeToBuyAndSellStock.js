245d1025bbd8cfff97b92f1ac7b591d9f959bc08
Djurre <djurreaikema@gmail.com>
2025-01-04 11:41:39 +0100

    04-01-2025

--- src/NeetCode/Sliding Window/BestTimeToBuyAndSellStock.js
+++ src/NeetCode/Sliding Window/BestTimeToBuyAndSellStock.js
@@ -20,6 +20,24 @@ class Solutions {
   }
 
   // All solutions
+  maxProfit040125(prices) {
+    let l = 0, r = 1;
+    let maxProfit = 0;
+
+    while (r < prices.length) {
+      if (prices[l] < prices[r]) {
+        const profit = prices[r] - prices[l];
+        maxProfit = Math.max(profit, maxProfit);
+      } else {
+        l = r;
+      }
+
+      r++;
+    }
+
+    return maxProfit;
+  }
+
   maxProfit030125(prices) {
     let l = 0, r =1;
     let maxProfit = 0;
