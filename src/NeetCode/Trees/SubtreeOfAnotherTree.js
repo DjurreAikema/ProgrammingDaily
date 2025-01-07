ab3bfffd6d2d490a85e36c6f8fafec8724f0ee15
d.aikema <djurreaikema@gmail.com>
2025-01-07 10:47:33 +0100

    07-01-2025

---
+++ src/NeetCode/Trees/SubtreeOfAnotherTree.js
@@ -0,0 +1,48 @@
+//noinspection Duplicates
+class Solutions {
+  // Best (so far)
+  isSubtree_best(root, subRoot) {
+    if (!subRoot) return true;
+    if (!root) return false;
+
+    if (this.sameTree_best(root, subRoot)) return true;
+    return (
+      this.isSubtree_best(root.left, subRoot)
+      || this.isSubtree_best(root.right, subRoot)
+    );
+  }
+
+  sameTree_best(root, subRoot) {
+    if (!root && !subRoot) return true;
+    if (root && subRoot && root.val === subRoot.val) {
+      return (
+        this.sameTree_best(root.left, subRoot.left)
+        && this.sameTree_best(root.right, subRoot.right)
+      );
+    }
+    return false;
+  }
+
+  // All solutions
+  isSubtree070125(root, subRoot) {
+    if (!subRoot) return true;
+    if (!root) return false;
+
+    if (this.sameTree(root, subRoot)) return true;
+    return (
+      this.isSubtree(root.left, subRoot)
+      || this.isSubtree(root.right, subRoot)
+    );
+  }
+
+  sameTree070125(root, subRoot) {
+    if (!root && !subRoot) return true;
+    if (root && subRoot && root.val === subRoot.val) {
+      return (
+        this.sameTree(root.left, subRoot.left)
+        && this.sameTree(root.right, subRoot.right)
+      );
+    }
+    return false;
+  }
+}
