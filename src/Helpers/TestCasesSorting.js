bded2725efcb09d4beabfdc2c8fcf8f6733ec9fa
d.aikema <djurreaikema@gmail.com>
2025-01-07 13:27:53 +0100

    07-01-2025

---
+++ src/Helpers/TestCasesSorting.js
@@ -0,0 +1,6 @@
+/**
+ * @param {Solution} sol
+ */
+export function bubbleSortTestCases(sol) {
+  console.log('Sorted Array:', sol.bubbleSort([64, 34, 25, 12, 22, 11, 90]));
+}
---
+++ src/SortingAlgorithms/WorkingFile.js
@@ -0,0 +1,9 @@
+import {bubbleSortTestCases} from "../Helpers/TestCasesSorting.js";
+
+//noinspection Duplicates
+class Solution {
+
+}
+
+
+const sol = new Solution();
