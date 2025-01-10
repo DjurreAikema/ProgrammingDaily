dd58826b1b986a23bc6573c948ecc0d91d8d6527
d.aikema <djurreaikema@gmail.com>
2025-01-10 09:48:17 +0100

    10-01-2025

--- src/NeetCode/Trees/DiameterOfBinaryTree.js
+++ src/NeetCode/Trees/DiameterOfBinaryTree.js
@@ -31,6 +31,36 @@ class Solutions {
   }
 
   // All solutions
+  diameterOfBinaryTree100125(root) {
+    const stack = [root];
+    const seen = new Map();
+    seen.set(null, [0, 0]);
+
+    while (stack.length) {
+      let node = stack[stack.length - 1];
+      if (node.left && !seen.has(node.left)) {
+        stack.push(node.left);
+      } else if (node.right && !seen.has(node.right)) {
+        stack.push(node.right);
+      } else {
+        node = stack.pop();
+
+        let [leftDepth, leftDiameter] = seen.get(node.left);
+        let [rightDepth, rightDiameter] = seen.get(node.right);
+
+        let height = 1 + Math.max(leftDepth, rightDepth);
+        let diameter = Math.max(
+          leftDepth + rightDepth,
+          Math.max(leftDiameter, rightDiameter)
+        );
+
+        seen.set(node, [height, diameter]);
+      }
+    }
+
+    return seen.get(root)[1];
+  }
+
   diameterOfBinaryTree070125(root) {
     const stack = [root];
     let seen = new Map();
