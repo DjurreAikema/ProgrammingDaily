692d13612e7b3dcd51718d1f50a78f83430e4c1a
d.aikema <djurreaikema@gmail.com>
2025-01-14 09:37:22 +0100

    14-01-2025

--- src/NeetCode/Linked List/ReverseLinkedList.js
+++ src/NeetCode/Linked List/ReverseLinkedList.js
@@ -15,6 +15,21 @@ class Solutions {
   }
 
   // All solutions
+  reverseList140125(head) {
+    const dummy = {val: 0, next: head};
+    let node = head;
+    let prev = null;
+
+    while (node) {
+      const tempNext = node.next;
+      node.next = prev;
+      prev = node;
+      node = tempNext;
+    }
+
+    return dummy.next;
+  }
+
   reverseList271224(head) {
     let curr = head, prev = null;
 
