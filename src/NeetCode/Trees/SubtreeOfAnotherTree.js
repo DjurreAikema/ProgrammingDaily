eb2f969e391bf19003f38fbe4592021df2debd31
Djurre <djurreaikema@gmail.com>
2025-04-09 13:38:00 +0200

    09-04-2025

--- src/NeetCode/Trees/SubtreeOfAnotherTree.js
+++ src/NeetCode/Trees/SubtreeOfAnotherTree.js
@@ -24,6 +24,28 @@ class Solutions {
   }
 
   // All solutions
+  isSubtree090425(root, subRoot) {
+    if (!subRoot) return true;
+    if (!root) return false;
+
+    if (this.sameTree(root, subRoot)) return true;
+    return (
+      this.isSubtree(root.left, subRoot)
+      || this.isSubtree(root.right, subRoot)
+    );
+  }
+
+  sameTree090425(root, subRoot) {
+    if (!root && !subRoot) return true;
+    if (root && subRoot && root.val === subRoot.val) {
+      return (
+        this.sameTree(root.left, subRoot.left)
+        && this.sameTree(root.right, subRoot.right)
+      );
+    }
+    return false;
+  }
+
   isSubtree240225(root, subRoot) {
     if (!subRoot) return true;
     if (!root) return false;
