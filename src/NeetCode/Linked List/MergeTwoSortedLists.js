dcf829eb2bdc70dccc414198e44e80c3575d9037
Djurre <djurreaikema@gmail.com>
2025-04-28 22:25:26 +0200

    28-04-2025

--- src/NeetCode/Linked List/MergeTwoSortedLists.js
+++ src/NeetCode/Linked List/MergeTwoSortedLists.js
@@ -26,6 +26,31 @@ class Solutions {
   }
 
   // All solutions
+  mergeTwoLists_280425_hard(list1, list2) {
+    const dummy = {val: 0, next: null};
+    let node = dummy;
+
+    while (list1 && list2) {
+      if (list1.val < list2.val) {
+        node.next = list1;
+        list1 = list1.next;
+      } else {
+        node.next = list2;
+        list2 = list2.next;
+      }
+
+      node = node.next;
+    }
+
+    if (list1) {
+      node.next = list1;
+    } else {
+      node.next = list2;
+    }
+
+    return dummy.next;
+  }
+
   mergeTwoLists170125(list1, list2) {
     const dummy = {val: 0, next: null};
     let node = dummy;
