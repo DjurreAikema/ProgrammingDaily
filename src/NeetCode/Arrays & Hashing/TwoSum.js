fb278aead9effddd2029c3aa602c8d23c9027c04
d.aikema <djurreaikema@gmail.com>
2025-04-29 09:29:08 +0200

    29-04-2025

--- src/NeetCode/Arrays & Hashing/TwoSum.js
+++ src/NeetCode/Arrays & Hashing/TwoSum.js
@@ -17,6 +17,21 @@ class Solutions {
   }
 
   // All solutions
+  twoSum_290425_hard(nums, target) {
+    const prevMap = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+      const diff = target - nums[i];
+      if (prevMap.has(diff)) {
+        return [prevMap.get(diff), i];
+      }
+
+      prevMap.set(nums[i], i);
+    }
+
+    return [];
+  }
+
   twoSum221224(nums, target) {
     const prevMap = new Map();
 
