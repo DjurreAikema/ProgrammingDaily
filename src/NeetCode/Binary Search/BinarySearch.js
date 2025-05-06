b34ee4c94000a38306db6bb2a63e95633a75a167
d.aikema <djurreaikema@gmail.com>
2025-05-06 09:23:44 +0200

    06-05-2025

--- src/NeetCode/Binary Search/BinarySearch.js
+++ src/NeetCode/Binary Search/BinarySearch.js
@@ -21,6 +21,24 @@ class Solutions {
   }
 
   // All solutions
+  search_060525_good(nums, target) {
+    let l = 0, r = nums.length - 1;
+
+    while (l <= r) {
+      const m = l + Math.floor((r - l) / 2);
+
+      if (nums[m] > target) {
+        r = m - 1;
+      } else if (nums[m] < target) {
+        l = m + 1;
+      } else {
+        return m;
+      }
+    }
+
+    return -1;
+  }
+
   search_010525_good(nums, target) {
     let l = 0, r = nums.length - 1;
 
