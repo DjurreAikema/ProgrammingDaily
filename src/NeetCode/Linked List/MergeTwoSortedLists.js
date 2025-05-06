ee197702c7e8e353762d9fa03f6bfa93a4005dd8
d.aikema <djurreaikema@gmail.com>
2025-05-06 11:44:43 +0200

    06-05-2025

--- src/NeetCode/Linked List/MergeTwoSortedLists.js
+++ src/NeetCode/Linked List/MergeTwoSortedLists.js
@@ -26,6 +26,31 @@ class Solutions {
   }
 
   // All solutions
+  mergeTwoLists_060525_good(list1, list2) {
+    const dummy = {val: 0, next: null};
+    let curr = dummy;
+
+    while (list1 && list2) {
+      if (list1.val < list2.val) {
+        curr.next = list1;
+        list1 = list1.next;
+      } else {
+        curr.next = list2;
+        list2 = list2.next;
+      }
+
+      curr = curr.next;
+    }
+
+    if (list1) {
+      curr.next = list1;
+    } else {
+      curr.next = list2;
+    }
+
+    return dummy.next;
+  }
+
   mergeTwoLists_280425_hard(list1, list2) {
     const dummy = {val: 0, next: null};
     let node = dummy;
