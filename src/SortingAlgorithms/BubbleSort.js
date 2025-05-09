4c41e51b0d6245bad609d2fe07915625bda8051b
d.aikema <djurreaikema@gmail.com>
2025-05-09 10:41:46 +0200

    09-05-2025

--- src/SortingAlgorithms/BubbleSort.js
+++ src/SortingAlgorithms/BubbleSort.js
@@ -14,6 +14,17 @@ class Solutions {
   }
 
   // All solutions
+  bubbleSort_090525_good(nums) {
+    for (let i = 0; i < nums.length - 1; i++) {
+      for (let j = 0; j < nums.length - i - 1; j++) {
+        if (nums[j] > nums[j + 1])
+          [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]]
+      }
+    }
+
+    return nums;
+  }
+
   bubbleSort_240425_hard(nums) {
     for (let i = 0; i < nums.length - 1; i++) {
       for (let j = 0; j < nums.length - 1 - i; j++) {
