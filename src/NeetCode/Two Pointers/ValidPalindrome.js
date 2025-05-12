3022e06fdbcba5e306d9f4487732e7920702bc36
Djurre <djurreaikema@gmail.com>
2025-05-12 16:02:36 +0200

    12-05-2025

--- src/NeetCode/Two Pointers/ValidPalindrome.js
+++ src/NeetCode/Two Pointers/ValidPalindrome.js
@@ -19,6 +19,21 @@ class Solutions {
   }
 
   // All solutions
+  isPalindrome_120525_hard(s) {
+    let l = 0, r = s.length - 1;
+
+    while (l < r) {
+      while(l < r && !alphaNum(s[l])) l++;
+      while(r > l && !alphaNum(s[r])) r--;
+
+      if (s[l].toLowerCase() !== s[r].toLowerCase()) return false;
+      l++;
+      r--;
+    }
+
+    return true;
+  }
+
   isPalindrome100125(s) {
     let l = 0, r = s.length - 1;
 
