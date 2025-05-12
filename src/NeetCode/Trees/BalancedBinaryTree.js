63fdfc46443ae28aee6221008b93a7f25b84cc51
Djurre <djurreaikema@gmail.com>
2025-05-12 16:11:16 +0200

    12-05-2025

--- src/NeetCode/Trees/BalancedBinaryTree.js
+++ src/NeetCode/Trees/BalancedBinaryTree.js
@@ -32,6 +32,36 @@ class Solutions {
   }
 
   // All solutions
+  isBalanced_120525_hard(root) {
+    if (root === null) return true;
+
+    let stack = [root];
+    let seen = new Map();
+    seen.set(null, 0);
+
+    while (stack.length) {
+      let node = stack[stack.length - 1];
+
+      if (node.left && !seen.has(node.left)) {
+        stack.push(node.left);
+      } else if (node.right && !seen.has(node.right)) {
+        stack.push(node.right);
+      } else {
+        node = stack.pop();
+
+        let leftHeight = seen.get(node.left);
+        let rightHeight = seen.get(node.right);
+
+        if (Math.abs(leftHeight - rightHeight) > 1) return false;
+
+        const height = 1 + Math.max(leftHeight, rightHeight);
+        seen.set(node, height);
+      }
+    }
+
+    return true;
+  }
+
   isBalanced030425(root) {
     if (!root) return true;
 
