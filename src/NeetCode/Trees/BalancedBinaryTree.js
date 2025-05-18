86fd9f287fb3b0f38ede7a42dd72baff02e802b5
Djurre <djurreaikema@gmail.com>
2025-05-18 13:08:06 +0200

    18-05-2025

--- src/NeetCode/Trees/BalancedBinaryTree.js
+++ src/NeetCode/Trees/BalancedBinaryTree.js
@@ -32,6 +32,36 @@ class Solutions {
   }
 
   // All solutions
+  isBalanced_180525_good(root) {
+    if (!root) return true;
+
+    const stack = [root];
+    const seen = new Map();
+    seen.set(null, 0);
+
+    while (stack.length) {
+      let node = stack[stack.length - 1];
+
+      if (node.left && !seen.has(node.left)) {
+        stack.push(node.left);
+      } else if (node.right && !seen.has(node.right)) {
+        stack.push(node.right);
+      } else {
+        node = stack.pop();
+
+        let leftHeight = seen.get(node.left);
+        let rightHeight = seen.get(node.right);
+
+        if (Math.abs(leftHeight - rightHeight) > 1) return false;
+
+        let height = 1 + Math.max(leftHeight, rightHeight);
+        seen.set(node, height);
+      }
+    }
+
+    return true;
+  }
+
   isBalanced_150525_good(root) {
     if (!root) return true;
 
