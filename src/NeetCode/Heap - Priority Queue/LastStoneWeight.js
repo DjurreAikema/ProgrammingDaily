0f88a4f10120d261b3b772713564f7180ed4fcd6
Djurre <djurreaikema@gmail.com>
2025-05-18 13:03:41 +0200

    18-05-2025

--- src/NeetCode/Heap - Priority Queue/LastStoneWeight.js
+++ src/NeetCode/Heap - Priority Queue/LastStoneWeight.js
@@ -1,4 +1,6 @@
 //noinspection Duplicates
+import {MaxPriorityQueue} from "@datastructures-js/priority-queue";
+
 class Solutions {
   // Best (so far)
   lastStoneWeight_best(stones) {
@@ -21,6 +23,25 @@ class Solutions {
   }
 
   // All solutions
+  lastStoneWeight_185025_hard(stones) {
+    this.maxHeap = new MaxPriorityQueue();
+
+    for (const stone of stones) {
+      this.maxHeap.enqueue(stone);
+    }
+
+    while (this.maxHeap.size() > 1) {
+      let stone1 = this.maxHeap.dequeue();
+      let stone2 = this.maxHeap.dequeue();
+
+      if (stone1 !== stone2) {
+        this.maxHeap.enqueue(stone1 - stone2);
+      }
+    }
+
+    return this.maxHeap.size() === 1 ? this.maxHeap.dequeue() : 0;
+  }
+
   lastStoneWeight_150525_soloSolved(stones) {
     const maxHeap = new MaxPriorityQueue;
 
