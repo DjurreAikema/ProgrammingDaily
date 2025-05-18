e69371b0c3821336b9248bd459bc4701083f1d23
Djurre <djurreaikema@gmail.com>
2025-05-18 12:58:24 +0200

    18-05-2025

--- src/NeetCode/Binary Search/BinarySearch.js
+++ src/NeetCode/Binary Search/BinarySearch.js
@@ -21,6 +21,24 @@ class Solutions {
   }
 
   // All solutions
+  search_180525_good(nums, target) {
+    let l = 0, r = nums.length - 1;
+
+    while (l <= r) {
+      const m = l + Math.floor((r - l) / 2);
+
+      if (nums[m] > target) {
+        r = m - 1;
+      } else if (nums[m] < target) {
+        l = m + 1;
+      } else {
+        return m;
+      }
+    }
+
+    return -1;
+  }
+
   search_110525_good(nums, target) {
     let l = 0, r = nums.length - 1;
 
