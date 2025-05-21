91d7a3a471648a5d5c5d11a93c3638fb2d79c298
Djurre <djurreaikema@gmail.com>
2025-05-21 10:02:42 +0200

    21-05-2025

--- src/Helpers/TestCases.js
+++ src/Helpers/TestCases.js
@@ -98,6 +98,7 @@ export function maximumDepthOfBinaryTreeTestCases(sol) {
 export function validPalindromeTestCases(sol) {
   console.log(sol.isPalindrome("Was it a car or a cat I saw?"), "True");
   console.log(sol.isPalindrome("tab a cat"), "False");
+  console.log(sol.isPalindrome("ab"), "False");
 }
 
 /**
