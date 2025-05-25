6f208ba3f99cab2b756b85354b90b2d591fc84e5
Djurre <djurreaikema@gmail.com>
2025-05-25 11:24:12 +0200

    25-05-2025

--- src/NeetCode/Linked List/ReverseLinkedList.js
+++ src/NeetCode/Linked List/ReverseLinkedList.js
@@ -15,6 +15,19 @@ class Solutions {
   }
 
   // All solutions
+  reverseList_250525_hard(head) {
+    let prev = null, curr = head;
+
+    while (curr) {
+      let tempNext = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = tempNext;
+    }
+
+    return prev;
+  }
+
   reverseList_210525_hard(head) {
     let prev = null, curr = head;
 
