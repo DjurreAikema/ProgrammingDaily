a2268e0b73947ef554c2ff8fbc0b3fbb8fdbbe0b
d.aikema <djurreaikema@gmail.com>
2025-06-06 16:29:01 +0200

    06-06-2025

--- src/NeetCode/Heap - Priority Queue/LastStoneWeight.js
+++ src/NeetCode/Heap - Priority Queue/LastStoneWeight.js
@@ -23,6 +23,25 @@ class Solutions {
   }
 
   // All solutions
+  lastStoneWeight_060625_good(stones) {
+    const maxHeap = new MaxPriorityQueue();
+
+    for (const stone of stones) {
+      maxHeap.enqueue(stone);
+    }
+
+    while (maxHeap.size() > 1) {
+      const stone1 = maxHeap.dequeue();
+      const stone2 = maxHeap.dequeue();
+
+      if (stone1 !== stone2) {
+      }
+      maxHeap.enqueue(stone1 - stone2);
+    }
+
+    return maxHeap.size() === 1 ? maxHeap.dequeue() : 0;
+  }
+
   lastStoneWeight_280525_good(stones) {
     const maxHeap = new MaxPriorityQueue();
 
