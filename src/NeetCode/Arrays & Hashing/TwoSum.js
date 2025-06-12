399871671a64fedcf8d01597193d2c43e686b3cb
d.aikema <djurreaikema@gmail.com>
2025-06-12 09:18:11 +0200

    12-06-2025

--- src/NeetCode/Arrays & Hashing/TwoSum.js
+++ src/NeetCode/Arrays & Hashing/TwoSum.js
@@ -17,6 +17,19 @@ class Solutions {
   }
 
   // All solutions
+  twoSum_120625_good(nums, target) {
+    const prevMap = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+      const diff = target - nums[i];
+
+      if (prevMap.has(diff))
+        return [prevMap.get(diff), i];
+
+      prevMap.set(nums[i], i);
+    }
+  }
+
   twoSum_280525_good(nums, target) {
     const prevMap = new Map();
 
