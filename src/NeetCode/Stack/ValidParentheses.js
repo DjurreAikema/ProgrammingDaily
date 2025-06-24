3aa94dbf8036cb4c432138b37f7b2eda41e58dd9
d.aikema <djurreaikema@gmail.com>
2025-06-24 13:54:37 +0200

    24-06-2025

--- src/NeetCode/Stack/ValidParentheses.js
+++ src/NeetCode/Stack/ValidParentheses.js
@@ -26,6 +26,29 @@ class Solutions {
   }
 
   // All solutions
+  isValid_240625_hard(s) {
+    const stack = [];
+    const closeToOpen = {
+      ')': '(',
+      '}': '{',
+      ']': '['
+    }
+
+    for (let c of s) {
+      if (closeToOpen[c]) {
+        if (stack.length > 0 && stack[stack.length - 1] === closeToOpen[c]) {
+          stack.pop();
+        } else {
+          return false
+        }
+      } else {
+        stack.push(c);
+      }
+    }
+
+    return stack.length === 0;
+  }
+
   isValid_120525_hard(s) {
     const stack = [];
     const closeToOpen = {
