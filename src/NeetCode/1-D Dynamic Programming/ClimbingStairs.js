8b40935a5c97cce9633e3b2477c06ab1639ddb8d
d.aikema <djurreaikema@gmail.com>
2025-06-26 09:23:34 +0200

    26-06-2025

--- src/NeetCode/1-D Dynamic Programming/ClimbingStairs.js
+++ src/NeetCode/1-D Dynamic Programming/ClimbingStairs.js
@@ -12,6 +12,16 @@ class Solutions {
   }
 
   // All solutions
+  climbStairs_260625_good(n) {
+    const cache = new Array(n).fill(-1);
+    const dfs = (i) => {
+      if (i >= n) return i === n;
+      if (cache[i] !== -1) return cache[i];
+      return cache[i] = dfs(i + 1) + dfs(i + 2);
+    }
+    return dfs(0);
+  }
+
   climbStairs_090625_good(n) {
     const cache = new Int32Array(n).fill(-1);
     const dfs = (i) => {
