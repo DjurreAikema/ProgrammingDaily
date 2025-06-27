b5168710d738fa28e4585a345c0c3a03eeba2d6a
d.aikema <djurreaikema@gmail.com>
2025-06-27 09:19:07 +0200

    27-06-2025

--- src/NeetCode/Bit Manipulation/NumberOfOneBits.js
+++ src/NeetCode/Bit Manipulation/NumberOfOneBits.js
@@ -11,7 +11,16 @@ class Solutions {
   }
 
   // All solutions
-  hammingWeight_first(n) {
+  hammingWeight_270625_good(n) {
+    let res = 0;
+    while (n !== 0) {
+      n &= n - 1;
+      res++;
+    }
+    return res;
+  }
+
+  hammingWeight_260625_first(n) {
     let res = 0;
     while (n !== 0) {
       n &= n - 1;
