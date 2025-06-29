beca0f1d556b55402f5ccb6ac9061a939ef79086
Djurre <djurreaikema@gmail.com>
2025-06-29 11:47:25 +0200

    29-06-2025

--- src/NeetCode/Bit Manipulation/SingleNumber.js
+++ src/NeetCode/Bit Manipulation/SingleNumber.js
@@ -10,6 +10,14 @@ class Solutions {
   }
 
   // All solutions
+  singleNumber_290625_good(nums) {
+    let res = 0;
+    for (const num of nums) {
+      res ^= num;
+    }
+    return res;
+  }
+
   singleNumber_270625_good(nums) {
     let res = 0;
     for (const num of nums) {
