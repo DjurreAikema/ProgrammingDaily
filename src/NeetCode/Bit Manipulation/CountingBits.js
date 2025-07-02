a2b0917f439d939ae555987900f2bf279efa76f8
Djurre <djurreaikema@gmail.com>
2025-07-02 21:26:39 +0200

    02-07-2025

--- src/NeetCode/Bit Manipulation/CountingBits.js
+++ src/NeetCode/Bit Manipulation/CountingBits.js
@@ -10,6 +10,14 @@ class Solutions {
   }
 
   // All solutions
+  countBits_020725_hard(n) {
+    let dp = new Array(n + 1).fill(0);
+    for (let i = 1; i <=n; i++) {
+      dp[i] = dp[i >> 1] + (i & 1);
+    }
+    return dp;
+  }
+
   countBits_010725_first_soloSolve(n) {
     const out = [];
 
