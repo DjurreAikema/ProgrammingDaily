384d0b45923e43be60e41fd1c27d48308e7da8a9
d.aikema <djurreaikema@gmail.com>
2025-08-05 09:18:12 +0200

    05-08-2025

--- src/NeetCode/Bit Manipulation/NumberOfOneBits.js
+++ src/NeetCode/Bit Manipulation/NumberOfOneBits.js
@@ -2,6 +2,19 @@
 class Solutions {
   // Best (so far)
   hammingWeight_best(n) {
+    let res = 0; // Count of 1-bits we've found
+    while (n !== 0) { // Keep going until all 1-bits are removed
+
+      // THE MAGIC TRICK: Remove the rightmost 1-bit from n
+      // This operation removes exactly ONE 1-bit each time
+      n &= n - 1;
+      res++;
+    }
+    return res;
+  }
+
+  // All solutions
+  hammingWeight_050825_good(n) {
     let res = 0;
     while (n !== 0) {
       n &= n - 1;
@@ -10,7 +23,6 @@ class Solutions {
     return res;
   }
 
-  // All solutions
   hammingWeight_290725_good(n) {
     let res = 0;
     while (n !== 0) {
