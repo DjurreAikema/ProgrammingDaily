3bf405a0636a2a933b82f2c35ecc6456fc11bd35
d.aikema <djurreaikema@gmail.com>
2025-08-05 09:09:38 +0200

    05-08-2025

--- src/NeetCode/Bit Manipulation/CountingBits.js
+++ src/NeetCode/Bit Manipulation/CountingBits.js
@@ -25,6 +25,14 @@ class Solutions {
   }
 
   // All solutions
+  countBits_050825_good(n) {
+    let dp = new Array(n + 1).fill(0);
+    for (let i = 1; i <= n; i++) {
+      dp[i] = dp[i >> 1] + (i & 1);
+    }
+    return dp;
+  }
+
   countBits_030825_hard(n) {
     let dp = new Array(n + 1).fill(0);
     for (let i = 1; i <= n; i++) {
