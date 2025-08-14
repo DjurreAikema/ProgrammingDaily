c3159803af7c6b1eea12109eaf3941eeab4485d0
d.aikema <djurreaikema@gmail.com>
2025-08-14 09:20:17 +0200

    14-08-2025

--- src/NeetCode/Arrays & Hashing/ValidAnagram.js
+++ src/NeetCode/Arrays & Hashing/ValidAnagram.js
@@ -18,6 +18,18 @@ class Solutions {
   }
 
   // All solutions
+  isAnagram_140825_easy(s, t) {
+    if (s.length !== t.length) return false;
+
+    const count = new Array(26).fill(0);
+    for (let i = 0; i <= s.length; i++) {
+      count[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+      count[t.charCodeAt(i) - 'a'.charCodeAt(0)]--;
+    }
+
+    return count.every(val => val === 0);
+  }
+
   isAnagram_240725_hard(s, t) {
     if (s.length !== t.length) return false;
 
