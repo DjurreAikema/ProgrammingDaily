af30705e54f087d47ad9dec6c4dcd2ac17559b54
Djurre <djurreaikema@gmail.com>
2025-08-24 20:49:04 +0200

    24-08-2025

--- src/NeetCode/Bit Manipulation/NumberOfOneBits.js
+++ src/NeetCode/Bit Manipulation/NumberOfOneBits.js
@@ -14,6 +14,15 @@ class Solutions {
   }
 
   // All solutions
+  hammingWeight_240825_hard(n) {
+    let res = 0;
+    while (n !== 0) {
+      n &= n - 1;
+      res++;
+    }
+    return res;
+  }
+
   hammingWeight_190825_hard(n) {
     let res = 0;
     while (n !== 0) {
