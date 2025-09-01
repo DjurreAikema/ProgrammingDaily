58cb0cd5407071a9a98cd3a1a7fc1903f23e4758
d.aikema <d.aikema@arox.nl>
2025-09-01 14:58:24 +0200

    01-09-2025

--- src/NeetCode/Trees/BalancedBinaryTree.js
+++ src/NeetCode/Trees/BalancedBinaryTree.js
@@ -32,6 +32,36 @@ class Solutions {
   }
 
   // All solutions
+  isBalanced_010925_good(root) {
+    if (!root) return true;
+
+    const stack = [root];
+    const seen = new Map();
+    seen.set(null, 0);
+
+    while (stack.length) {
+      let node = stack[stack.length - 1];
+
+      if (node.left && !seen.has(node.left)) {
+        stack.push(node.left);
+      } else if (node.right && !seen.has(node.right)) {
+        stack.push(node.right);
+      } else {
+        node = stack.pop();
+
+        let lHeight = seen.get(node.left);
+        let rHeight = seen.get(node.right);
+
+        if (Math.abs(lHeight - rHeight) > 1) return false;
+
+        const height = 1 + Math.max(lHeight, rHeight);
+        seen.set(node, height);
+      }
+    }
+
+    return true;
+  }
+
   isBalanced_210825_good(root) {
     if (!root) return true;
 
