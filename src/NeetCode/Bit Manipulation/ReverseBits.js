90a1a54548a3c1222403abb6ff2295c91823e370
d.aikema <d.aikema@arox.nl>
2025-09-02 10:37:45 +0200

    02-09-2025

--- src/NeetCode/Bit Manipulation/ReverseBits.js
+++ src/NeetCode/Bit Manipulation/ReverseBits.js
@@ -24,6 +24,15 @@ class Solutions {
   }
 
   // All solutions
+  reverseBits_020925_good(n) {
+    let res = 0;
+    for (let i = 0; i < 32; i++) {
+      const bit = (n >>> i) & 1;
+      res += bit << (31 - i);
+    }
+    return res >>> 0;
+  }
+
   reverseBits_010925_hard(n) {
     let res = 0;
     for (let i = 1; i <= 32; i++) {
