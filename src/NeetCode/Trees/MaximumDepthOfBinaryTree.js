160697b361cb95f6cab74aa7b61bd2ceb35e2d5c
d.aikema <djurreaikema@gmail.com>
2025-09-10 16:36:59 +0200

    10-09-2025

--- src/NeetCode/Trees/MaximumDepthOfBinaryTree.js
+++ src/NeetCode/Trees/MaximumDepthOfBinaryTree.js
@@ -21,6 +21,25 @@ class Solutions {
   }
 
   // All solutions
+  maxDepth_100925_easy(root) {
+    const stack = [[root, 1]];
+    let maxDepth = 0;
+
+    while (stack.length) {
+      const curr = stack.pop();
+      const node = curr[0];
+      const depth = curr[1];
+
+      if (node) {
+        maxDepth = Math.max(depth, maxDepth);
+        stack.push([node.left, depth + 1]);
+        stack.push([node.right, depth + 1]);
+      }
+    }
+
+    return maxDepth;
+  }
+
   maxDepth_210825_good(root) {
     const stack = [[root, 1]];
     let maxDepth = 0;
