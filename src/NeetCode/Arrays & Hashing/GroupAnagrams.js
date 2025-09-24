74623553ee0f95a755bfd6c048cf2b549659d201
Djurre <djurreaikema@gmail.com>
2025-09-24 15:14:55 +0200

    24-09-2025

--- src/NeetCode/Arrays & Hashing/GroupAnagrams.js
+++ src/NeetCode/Arrays & Hashing/GroupAnagrams.js
@@ -1,7 +1,7 @@
 //noinspection Duplicates
 class Solutions {
   // Best (so far)
-  groupAnagrams_besy(strs) {
+  groupAnagrams_best(strs) {
     const res = {};
     for (let s of strs) {
       const count = new Array(26).fill(0);
@@ -19,6 +19,25 @@ class Solutions {
   }
 
   // All solutions
+  groupAnagrams_240925_hard(strs) {
+    let res = {};
+
+    for (let s of strs) {
+      const count = new Array(26).fill(0);
+      for (let c in s) {
+        count[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+      }
+
+      const key = count.join(",");
+      if (!res[key]) {
+        res[key] = [];
+      }
+      res[key].push(s);
+    }
+
+    return Object.values(res);
+  }
+
   groupAnagrams_first_cheated(strs) {
     const res = {};
     for (let s of strs) {
