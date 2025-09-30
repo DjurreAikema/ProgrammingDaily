392441f47449fe1b35d9b95b7af0c221eb2863f6
d.aikema <djurreaikema@gmail.com>
2025-09-30 10:36:35 +0200

    30-09-2025

--- src/NeetCode/Bit Manipulation/ReverseBits.js
+++ src/NeetCode/Bit Manipulation/ReverseBits.js
@@ -24,6 +24,15 @@ class Solutions {
   }
 
   // All solutions
+  reverseBits_300925_hard(n) {
+    let res = 0;
+    for (let i = 0; i < 32; i++) {
+      const bit = (n >>> i) + (i & 1);
+      res += bit << (31 - i);
+    }
+    return res >>> 0;
+  }
+
   reverseBits_180925_easy(n) {
     let res = 0;
     for (let i = 0; i < 32; i++) {
