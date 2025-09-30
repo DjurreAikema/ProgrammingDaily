5655b28b4550920124f1f5215272f21a3e66446f
d.aikema <djurreaikema@gmail.com>
2025-09-30 17:08:42 +0200

    30-09-2025

--- src/NeetCode/Arrays & Hashing/GroupAnagrams.js
+++ src/NeetCode/Arrays & Hashing/GroupAnagrams.js
@@ -19,6 +19,24 @@ class Solutions {
   }
 
   // All solutions
+  groupAnagrams_300925_good(strs) {
+    let res = {};
+    for (const s of strs) {
+      const count = new Array(26).fill(0);
+      for (const c of s) {
+        count[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+      }
+
+      const key = count.join(',');
+      if (!res[key]) {
+        res[key] = [];
+      }
+      res[key].push(s);
+    }
+
+    return Object.values(res);
+  }
+
   groupAnagrams_270925_good(strs) {
     let res = {};
     for (const s of strs) {
