4a7b381a05a8d49329ee2acde43b29a66c6a70a3
d.aikema <djurreaikema@gmail.com>
2025-10-23 09:28:41 +0200

    23-10-2025

--- src/NeetCode/Arrays & Hashing/GroupAnagrams.js
+++ src/NeetCode/Arrays & Hashing/GroupAnagrams.js
@@ -19,6 +19,25 @@ class Solutions {
   }
 
   // All solutions
+  groupAnagrams_231025_easy(strs) {
+    let res = {};
+    for (let s of strs) {
+      const count = new Array(26).fill(0);
+      for (let c of s) {
+        count[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+      }
+
+      const key = count.join(",");
+      if (!res[key]) {
+        res[key] = [];
+      }
+
+      res[key].push(s);
+    }
+
+    return Object.values(res);
+  }
+
   groupAnagrams_300925_good(strs) {
     let res = {};
     for (const s of strs) {
