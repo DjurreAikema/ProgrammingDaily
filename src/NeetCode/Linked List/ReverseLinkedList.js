23b239547b41d435a49d77f7240bedf377668d12
d.aikema <djurreaikema@gmail.com>
2026-01-16 08:59:45 +0100

    16-01-2026

--- src/NeetCode/Linked List/ReverseLinkedList.js
+++ src/NeetCode/Linked List/ReverseLinkedList.js
@@ -15,6 +15,19 @@ class Solutions {
   }
 
   // All solutions
+  reverseList_160126_hard(head) {
+    let prev = null, curr = head;
+
+    while (curr) {
+      let tempNext = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = tempNext;
+    }
+
+    return prev;
+  }
+
   reverseList_211025_easy(head) {
     let curr = head;
     let prev = null;
