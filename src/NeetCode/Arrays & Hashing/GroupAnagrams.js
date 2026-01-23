c2161102e7ab9207588dd6a5aeda4681d2c717d9
d.aikema <djurreaikema@gmail.com>
2026-01-23 16:59:49 +0100

    23-01-2026

--- src/NeetCode/Arrays & Hashing/GroupAnagrams.js
+++ src/NeetCode/Arrays & Hashing/GroupAnagrams.js
@@ -19,6 +19,23 @@ class Solutions {
   }
 
   // All solutions
+  groupAnagrams_230126_hard(strs) {
+    let res = {};
+    for (let s of strs) {
+      const count = new Array(26).fill(0);
+      for (let c of s) {
+        count[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+      }
+
+      const key = count.join(",");
+      if (!res[key]) {
+        res[key] = [];
+      }
+      res[key].push(s);
+    }
+    return Object.values(res);
+  }
+
   groupAnagrams_231025_easy(strs) {
     let res = {};
     for (let s of strs) {
